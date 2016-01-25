36f7a94f29ab3a28fecce84b8865b6bf3cea5f54
Boltodiz <rammerstorfer.dietmar@gmx.at>
2016-01-25 16:29:41 +0100

    Edited Composition Scene

---
+++ Assets/Scripts/AutoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScript : MonoBehaviour {
+
+    public float speed = 1f;
+    ColorGenerator colorGenerator;
+    // Use this for initialization
+    void Start () {
+        colorGenerator = FindObjectOfType<ColorGenerator>();
+        Color color = colorGenerator.GenColor(1f, 1f);
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(transform.forward * Time.deltaTime);
+	}
+}
